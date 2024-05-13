858124964d32956ce59e4f6cf147565f4f5c63c8
Lord Camelot <245642627@qq.com>
2024-05-13 15:07:14 +0800

    submit 2024/5/13

--- src/DepthFirstSearch/LeetCode98.java
+++ src/DepthFirstSearch/LeetCode98.java
@@ -12,22 +12,22 @@ import java.util.LinkedList;
  */
 public class LeetCode98 {
     public boolean isValidBST(TreeNode root) {
-        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return preorder(root, Long.MIN_VALUE, Long.MAX_VALUE);
 //        return inorder(root);
     }
 
-    // 方法一：递归（如果该二叉树的左子树不为空，则左子树上所有节点的值均小于它的根节点的值； 若它的右子树不空，则右子树上所有节点的值均大于它的根节点的值；它的左右子树也为二叉搜索树）
-    private boolean dfs(TreeNode node, long lower, long upper) {
+    // 方法一：先序遍历-递归（如果该二叉树的左子树不为空，则左子树上所有节点的值均小于它的根节点的值； 若它的右子树不空，则右子树上所有节点的值均大于它的根节点的值；它的左右子树也为二叉搜索树）
+    private boolean preorder(TreeNode node, long lower, long upper) {
         if (node == null) {
             return true;
         }
         if (node.val <= lower || node.val >= upper) {
             return false;
         }
-        return dfs(node.left, lower, node.val) && dfs(node.right, node.val, upper);
+        return preorder(node.left, lower, node.val) && preorder(node.right, node.val, upper);
     }
 
-    // 方法二：中序遍历（二叉搜索树的中序遍历一定为顺序）
+    // 方法二：中序遍历-循环（二叉搜索树的中序遍历一定为顺序）
     private boolean inorder(TreeNode node) {
         Deque<TreeNode> stack = new LinkedList<>();
         double inorder = -Double.MAX_VALUE;
@@ -45,5 +45,23 @@ public class LeetCode98 {
             node = node.right;
         }
         return true;
+    }
+
+    private long pre = Long.MIN_VALUE;
+    private boolean inorder2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!inorder2(root.left) || root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return inorder2(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(1);
+        System.out.println(new LeetCode98().inorder2(root));;
     }
 }
