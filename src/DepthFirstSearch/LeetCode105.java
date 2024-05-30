ed7bd324f52197db31eb68edb2c2122d534f9401
Lord Camelot <245642627@qq.com>
2024-05-30 19:28:36 +0800

    submit 2024/5/30

---
+++ src/DepthFirstSearch/LeetCode105.java
@@ -0,0 +1,36 @@
+package DepthFirstSearch;
+
+import utils.TreeNode;
+
+import java.util.Arrays;
+
+/**
+ * @author Lord Camelot
+ * @date 2024/5/30
+ * @description LeetCode105 从前序与中序遍历序列构造二叉树
+ */
+public class LeetCode105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        if (n == 0) {
+            return null;
+        }
+        int leftSize = indexOf(inorder, preorder[0]);
+        int[] pre1 = Arrays.copyOfRange(preorder, 1, leftSize + 1);
+        int[] pre2 = Arrays.copyOfRange(preorder, leftSize + 1, n);
+        int[] in1 = Arrays.copyOfRange(inorder, 0, leftSize);
+        int[] in2 = Arrays.copyOfRange(inorder, 1 + leftSize, n);
+        TreeNode left = buildTree(pre1, in1);
+        TreeNode right = buildTree(pre2, in2);
+        return new TreeNode(preorder[0], left, right);
+
+    }
+
+    private int indexOf(int[] inorder, int val) {
+        for (int i = 0; ; i++) {
+            if (inorder[i] == val) {
+                return i;
+            }
+        }
+    }
+}
---
+++ src/DepthFirstSearch/LeetCode106.java
@@ -0,0 +1,35 @@
+package DepthFirstSearch;
+
+import utils.TreeNode;
+
+import java.util.Arrays;
+
+/**
+ * @author Lord Camelot
+ * @date 2024/5/30
+ * @description LeetCode106 从中序与后序遍历序列构造二叉树
+ */
+public class LeetCode106 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int n = inorder.length;
+        if (n == 0) {
+            return null;
+        }
+        int leftSize = indexOf(inorder, postorder[n - 1]);
+        int[] in1 = Arrays.copyOfRange(inorder, 0, leftSize);
+        int[] in2 = Arrays.copyOfRange(inorder, leftSize + 1, n);
+        int[] post1 = Arrays.copyOfRange(postorder, 0, leftSize);
+        int[] post2 = Arrays.copyOfRange(postorder, leftSize, n - 1);
+        TreeNode left = buildTree(in1, post1);
+        TreeNode right = buildTree(in2, post2);
+        return new TreeNode(postorder[n - 1], left, right);
+    }
+
+    private int indexOf(int[] inorder, int val) {
+        for (int i = 0; ; i++) {
+            if (inorder[i] == val) {
+                return i;
+            }
+        }
+    }
+}
