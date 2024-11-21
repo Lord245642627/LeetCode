a9a70ae987f715a943b0dc66a31a132efb5de794
lord camelot <245642627@qq.com>
2024-11-21 23:28:17 +0800

    init 2024/11/21

--- src/Test.java
+++ src/Test.java
@@ -3,12 +3,9 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<List<Integer>> list = new ArrayList<>();
-        list = List.of(List.of(), List.of(), List.of(1, 2, 3));
-        list.forEach(System.out::println);
-        String str = "";
-        str.isBlank();
-        Integer[] intArray = new Integer[10];
-        intArray[1] = 10;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World");
+        int i = 1;
+        String.valueOf(i);
     }
 }
