da4c60f2cabe9a7f5809889bc7934fd085b13aa1
lord camelot <245642627@qq.com>
2024-11-21 23:28:08 +0800

    init 2024/11/21

---
+++ src/Backtrack/LeetCode257.java
@@ -0,0 +1,43 @@
+package Backtrack;
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lord
+ * @date 2024/11/21
+ * @description LeetCode257 二叉树的所有路径
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * 叶子节点 是指没有子节点的节点。
+ */
+public class LeetCode257 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        constructPaths(root, "", paths);
+        return paths;
+    }
+
+    public void constructPaths(TreeNode root, String path, List<String> paths) {
+        if (root != null) {
+            StringBuffer pathSB = new StringBuffer(path);
+            pathSB.append(Integer.toString(root.val));
+            if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                paths.add(pathSB.toString());  // 把路径加入到答案中
+            } else {
+                pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                constructPaths(root.left, pathSB.toString(), paths);
+                constructPaths(root.right, pathSB.toString(), paths);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        System.out.println(new LeetCode257().binaryTreePaths(root));
+    }
+}
