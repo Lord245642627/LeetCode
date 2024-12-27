9e929147d261bca03fda67f9b2180653fecddc98
lord camelot <245642627@qq.com>
2024-12-27 22:13:25 +0800

    init 2024/12/27

---
+++ src/BreadthFirstSearch/LeetCode357.java
@@ -0,0 +1,24 @@
+package BreadthFirstSearch;
+
+/**
+ * @author lord
+ * @date 2024/12/27
+ * @description LeetCode357 统计各位数字都不同的数字个数
+ * 给你一个整数 n ，统计并返回各位数字都不同的数字 x 的个数，其中 0 <= x < 10n 。
+ */
+public class LeetCode357 {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int res = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++) {
+            cur *= 9 - i;
+            res += cur;
+        }
+        return res;
+    }
+}
