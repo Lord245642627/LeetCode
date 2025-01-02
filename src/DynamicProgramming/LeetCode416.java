65781ade138958dea3cc0506b0e7ee24272d2efa
lord camelot <245642627@qq.com>
2025-01-02 17:33:34 +0800

    init 2025/1/2

---
+++ src/DynamicProgramming/LeetCode416.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+/**
+ * @author lord
+ * @date 2025/1/2
+ * @description LeetCode416 分割等和子集
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ */
+public class LeetCode416 {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum /=  2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[sum];
+    }
+}
