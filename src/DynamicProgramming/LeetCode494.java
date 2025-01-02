197304de8713cfa112fb906c41d6242e7a511dde
lord camelot <245642627@qq.com>
2025-01-02 16:28:16 +0800

    init 2025/1/2

--- src/DynamicProgramming/LeetCode494.java
+++ src/DynamicProgramming/LeetCode494.java
@@ -23,17 +23,15 @@ public class LeetCode494 {
             return 0;
         }
         target /= 2;
-        int[][] dp = new int[nums.length + 1][target + 1];
-        dp[0][0] = 1;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < target + 1; j++) {
-                if (nums[i] > j) {
-                    dp[i + 1][j] = dp[i][j];
-                } else {
-                    dp[i + 1][j] = dp[i][j] + dp[i][j - nums[i]];
+            for (int j = target; j >= 0; j--) {
+                if (nums[i] <= j) {
+                    dp[j] += dp[j - nums[i]];
                 }
             }
         }
-        return dp[nums.length][target];
+        return dp[target];
     }
 }
