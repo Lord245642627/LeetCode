d5fa2af5fb59ca8051a31d366923a87df7702baf
lord camelot <245642627@qq.com>
2025-01-03 22:01:30 +0800

    init 2025/1/3

---
+++ src/DynamicProgramming/LeetCode1092.java
@@ -0,0 +1,56 @@
+package DynamicProgramming;
+
+/**
+ * @author lord
+ * @date 2025/1/3
+ * @description LeetCode1092 最短公共超序列
+ * 给你两个字符串 str1 和 str2，返回同时以 str1 和 str2 作为 子序列 的最短字符串。如果答案不止一个，则可以返回满足条件的 任意一个 答案。
+ * 如果从字符串 t 中删除一些字符（也可能不删除），可以得到字符串 s ，那么 s 就是 t 的一个子序列。
+ */
+public class LeetCode1092 {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n1 = str1.length(), n2 = str2.length();
+        int[][] dp = new int[n1 + 1][n2 +1];
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // 逆序构造
+        int i = n1, j = n2;
+        while(i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i][j] == dp[i - 1][j] + 1) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+            } else if (dp[i][j] == dp[i][j - 1] + 1) {
+                sb.append(str2.charAt(j - 1));
+                j--;
+            }
+        }
+        while (i > 0) {
+            sb.append(str1.charAt(i - 1));
+            i--;
+        }
+        while (j > 0) {
+            sb.append(str2.charAt(j - 1));
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
