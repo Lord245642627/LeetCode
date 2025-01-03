aeb5d284b2e8aeefafa041009cc7dd373542b49a
lord camelot <245642627@qq.com>
2025-01-03 19:54:58 +0800

    init 2025/1/3

---
+++ src/DynamicProgramming/LeetCode583.java
@@ -0,0 +1,31 @@
+package DynamicProgramming;
+
+/**
+ * @author lord
+ * @date 2025/1/3
+ * @description LeetCode583 两个字符串的删除操作
+ * 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+ * 每步 可以删除任意一个字符串中的一个字符。
+ */
+public class LeetCode583 {
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[][] dp = new int[len1 +1 ][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= len2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
