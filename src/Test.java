834bea6a6929e96441aa12b27f0486b6c592a9d5
Lord Camelot <245642627@qq.com>
2025-08-21 20:06:27 +0800

    2025/8/21

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Test {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.append("Hello World");
         int i = 1;
         String.valueOf(i);
+
     }
 }
